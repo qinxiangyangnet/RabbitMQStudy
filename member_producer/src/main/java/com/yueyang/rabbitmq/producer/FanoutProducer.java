a54c773431ceb621a85d2119fbcd493589895ff1
qinxiangyang <qinxiangyang@xinhuamm.net>
2020-05-24 12:39:32 +0800

    幂等性问题采用messageId（伪代码）

--- member_producer/src/main/java/com/yueyang/rabbitmq/producer/FanoutProducer.java
+++ member_producer/src/main/java/com/yueyang/rabbitmq/producer/FanoutProducer.java
@@ -2,6 +2,9 @@ package com.yueyang.rabbitmq.producer;
 
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -34,8 +37,16 @@ public class FanoutProducer {
         jsonObject.put("timstamp", System.currentTimeMillis());
         String msg = "msg_" + UUID.randomUUID().toString();
         jsonObject.put("msg", msg);
+        String jsonString = jsonObject.toJSONString();
         //发送消息
-        amqpTemplate.convertAndSend(queueName, jsonObject.toJSONString());
+
+        //生产者发送消息的时候设置消息Id
+        Message message = MessageBuilder
+                .withBody(jsonString.getBytes())
+                .setContentType(MessageProperties.CONTENT_TYPE_JSON)
+                .setContentEncoding("utf-8")
+                .setMessageId(UUID.randomUUID() + "").build();
+        amqpTemplate.convertAndSend(queueName, message);
 
     }
 
--- msg_consumer/src/main/java/com/yueyang/rabbitmq/email/EmailConsumer.java
+++ msg_consumer/src/main/java/com/yueyang/rabbitmq/email/EmailConsumer.java
@@ -1,5 +1,8 @@
 package com.yueyang.rabbitmq.email;
 
+import com.alibaba.fastjson.JSONObject;
+import com.yueyang.rabbitmq.utils.HttpClientUtils;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -11,14 +14,34 @@ import org.springframework.stereotype.Component;
  * @create: 2020-05-24 10:55
  **/
 @Component
-@RabbitListener(queues = "new_fanout_email_queue")
+//@RabbitListener(queues = "new_fanout_email_queue")
 public class EmailConsumer {
 
 
-    @RabbitHandler
-    public void process(String msg) {
+    //    @RabbitHandler()
+    @RabbitListener(queues = "new_fanout_email_queue")
+    public void process(Message message) throws Exception {
 
-        System.out.println("邮件消费者获取生产者生成的消息:" + msg);
+        String messageId = message.getMessageProperties().getMessageId();
+        String msg = new String(message.getBody(), "utf-8");
+        System.out.println("messageId:" + messageId);
 
+//        if(messageId has  consumer){
+        //伪代码
+//            return;
+//        }
+        JSONObject jsonObject = JSONObject.parseObject(msg);
+        System.out.println("短信消费者获取生产者生成的消息:" + jsonObject.getString("msg"));
+
+        String email = jsonObject.getString("email");
+        String emailUrl = "http://localhost:8082/sendEmail?email=" + email;
+        JSONObject retult = HttpClientUtils.httpGet(emailUrl);
+        //如果调用第三方接口，怎么补偿
+        if (retult == null) {
+            throw new Exception("调用第三方接口异常");
+        }
+
+
+        System.out.println("调用第三方成功，result" + retult + "程序结束");
     }
 }
